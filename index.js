3adfc2cba5fbc51ecf24992a1528a527d652108c
OpportunityLiu <opportunity@live.in>
2018-08-09 15:28:50 +0800

    add div & class

--- index.js
+++ index.js
@@ -2,28 +2,21 @@ var fs = require('fs'),
   path = require('path'),
   _ = require('underscore');
 
-var filePath = path.join(__dirname, 'youku-template.html');
+var filePath = path.join(__dirname, 'youku-template.ejs');
 
-function youkuMaps(args, content) {
-
-  var template = fs.readFileSync(filePath).toString(),
-    options = {};
-
-  if (content.length) {
-    var cont = content.split('\n');
-    id = cont.shift();
-  }
+function youkuMaps(args)
+{
+  var template = fs.readFileSync(filePath).toString();
 
   // Output into 
   return _.template(template)({
-    id: id,
-    height: args[0] || 480,
-    width: args[1] || 400
+    id: args[0],
+    class: (args[1] || 'youku') + 'video-container'
   });
 }
 
 
 hexo.extend.tag.register('youku', youkuMaps, {
   async: true,
-  ends: true
+  ends: false
 });
